7f3af5973cdca9110b21e82dadef5cbcd8c05181
Tefan Haetami <tefanhaetami87@gmail.com>
2024-05-16 10:24:17 +0700

    chore: babel compiler

---
+++ src/datatable-reducer.js
@@ -0,0 +1,46 @@
+/**
+ * Caches the span of each unique value in the data array.
+ *
+ * @param {string} key - The key to store the span under.
+ * @param {Function} accessor - A function to access the value to compare in the data.
+ * @param {Object} spans - An object to store the spans.
+ * @param {Array} DATA - The data array to process.
+ */
+export function cacheSpan(key, accessor, spans, DATA) {
+  let i = 0;
+  DATA.forEach((data, index) => {
+    let currentValue = accessor(data);
+    let count = 1;
+
+    for (let j = index + 1; j < DATA.length; j++) {
+      if (currentValue !== accessor(DATA[j])) {
+        break;
+      }
+      count++;
+    }
+
+    spans[i] = spans[i] || {};
+    spans[i][key] = count;
+    i += count;
+  });
+}
+
+/**
+ * Retrieves the span for a given column and index.
+ *
+ * @param {string} col - The column to retrieve the span for.
+ * @param {number} index - The index to retrieve the span for.
+ * @param {Object} spans - The object storing the spans.
+ * @returns {number} The span for the given column and index, or undefined if no span is stored.
+ */
+export function getRowSpan(col, index, spans) {    
+  return spans[index]?.[col];
+}
+
+/**
+ * An object containing functions for managing a datatable.
+ */
+export const datatableReducer = {
+  cacheSpan,
+  getRowSpan
+}
---
+++ src/index.js
@@ -0,0 +1,58 @@
+/**
+ * Checks if the key pressed is a number key and prevents the event if it's not.
+ *
+ * @param {Event} ev - The keypress event.
+ * @returns {boolean} Returns true if the key pressed is a number key, false otherwise.
+ *
+ * @example
+ * // In an input field event listener
+ * inputField.addEventListener('keypress', onlyNumberKey);
+ */
+export function onlyNumberKey(ev) {
+  const ASCIICode = ev.which ? ev.which : ev.keyCode
+  if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57)) {
+    ev.preventDefault()
+    return false
+  }
+  return true
+}
+
+/**
+ * Accesses a property of an object using a dot notation string.
+ *
+ * @param {string} member - The dot notation string representing the path to a nested property.
+ * @param {Object} [context=this] - The object on which to perform the property access. Defaults to the current context (`this`).
+ * @returns {*} The value of the property at the end of the dot notation path.
+ *
+ * @example
+ * // returns "Hello, world!"
+ * memberAccessor("property1.property2.property3", { property1: { property2: { property3: "Hello, world!" } } });
+ */
+export function memberAccessor(member, context = this) {
+  return member.split('.').reduce((acc, curr) => acc[curr], context)
+}
+
+/**
+ * Converts a dot notation string into a nested object.
+ *
+ * @param {string} dotNotation - The dot notation string representing the path to a nested property.
+ * @param {*} value - The value to be assigned to the property at the end of the dot notation path.
+ * @returns {Object} The nested object created from the dot notation string.
+ *
+ * @example
+ * // returns { property1: { property2: { property3: "Hello, world!" } } }
+ * convertDotNotationToObject("property1.property2.property3", "Hello, world!");
+ */
+export function convertDotNotationToObject(dotNotation, value) {
+  const keys = dotNotation.split('.')
+  const nestedObject = keys.slice(0, -1).reduce((acc, key) => {
+    acc[key] = {}
+    return acc[key]
+  }, {})
+
+  nestedObject[keys[keys.length - 1]] = value
+
+  return nestedObject
+}
+
+export { datatableReducer } from './datatable-reducer.js';
